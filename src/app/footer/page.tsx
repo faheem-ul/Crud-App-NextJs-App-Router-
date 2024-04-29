cfbc63325160e1035a34059f26f1167b1138dc92
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-04-29 11:58:06 +0500

    project setup

---
+++ src/app/footer/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <div>this is footer page </div>;
+}
+
+export default Footer;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div>
+      Signup
+      <form>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="text" />
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
