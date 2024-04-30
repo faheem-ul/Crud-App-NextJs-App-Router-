ca53d8ccb6d6bc94e9728bec63d9b9cd1eefa18b
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-04-30 18:47:15 +0500

    route group, loading, and custom 404 page is working

---
+++ src/app/(home)/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <h1 className=" text-4xl text-green-600 font-bold h-[100vh] flex justify-center items-center">
+      About Us
+    </h1>
+  );
+}
+
+export default About;
---
+++ src/app/profile/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <h1 className=" text-4xl text-green-600 font-bold h-[100vh] flex justify-center items-center">
+      Loading
+    </h1>
+  );
+}
+
+export default Loading;
